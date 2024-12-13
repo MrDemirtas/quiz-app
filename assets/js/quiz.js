d2c55d997581d11915e0a902b7b6128581ff5a92
Furkan Demirtaş <furkan-demirtas@outlook.com>
2024-12-13 19:14:13 +0300

    dark mode localstroage'e bağlandı, quiz seçme butonların etkileşimleri eklendi

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,5 +1,4 @@
 const selectedTheme = document.querySelector(".switch");
-
 selectedTheme.addEventListener("change", () => darkMode({ param: selectedTheme.checked }));
 
 function darkMode({ param = "null" }) {
@@ -20,6 +19,29 @@ function darkMode({ param = "null" }) {
 async function init() {
   const currentTheme = darkMode({ param: localStorage.getItem("dark-mode") });
   currentTheme === "on" ? (selectedTheme.checked = true) : (selectedTheme.checked = false);
+  const { quizzes } = await fetch("/assets/json/data.json").then((x) => x.json());
+
+  document.querySelectorAll(".quizTypeButton").forEach((element) => {
+    element.addEventListener("click", quizTypeButtonHandle);
+  });
+
+  function quizTypeButtonHandle() {
+    const quizType = this.getAttribute("data-id");
+    const questions = quizzes.find((x) => x.title == quizType);
+    if (quizType === "HTML") {
+      startQuiz(questions);
+    } else if (quizType === "CSS") {
+      startQuiz(questions);
+    } else if (quizType === "JavaScript") {
+      startQuiz(questions);
+    } else if (quizType === "Erişilebilirlik") {
+      startQuiz(questions);
+    }
+  }
 }
 
 init();
+
+function startQuiz(quizzes) {
+  console.log(quizzes);
+}
